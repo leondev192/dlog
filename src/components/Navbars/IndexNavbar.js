1b52b8e23c2e6059c7c3a0a54ca5231884d6faa8
daovanquyet1909 <daovanquyet1909@gmail.com>
2024-11-13 00:59:16 +0700

    adad

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -71,7 +71,7 @@ function IndexNavbar() {
             target="_blank"
             title="Coded by Creative Tim"
           >
-            Paper Kit React
+            Dlog Logistics
           </NavbarBrand>
           <button
             aria-expanded={navbarCollapse}
